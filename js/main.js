20b9123397bc2da2722406470152a6378effc1be
webmanufactory <bubunya@yandex.ru>
2020-10-19 15:05:19 +0300

    New changes

--- js/main.js
+++ js/main.js
@@ -26,19 +26,12 @@ $(document).ready(function(){
                 {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
                 },
                 {
                 breakpoint: 769,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-                },
-                {
-                breakpoint: 481,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
