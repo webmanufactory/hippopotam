050a5bbc2c747b797ed9a03aa5176855a791b6c7
webmanufactory <bubunya@yandex.ru>
2020-10-19 03:02:32 +0300

    new web page

---
+++ js/main.js
@@ -0,0 +1,111 @@
+let carousel = $(".services__galery");
+let carouselItem = $(".galery__link");
+let carouselImage = $(".galery__image");
+let menu = $('.icon');
+
+$(document).ready(function(){
+    if (parseInt(window.innerWidth) <= 1024){
+        menu.click(function(e){
+            e.preventDefault();
+            $('.nav__list').slideToggle();
+        });
+    };
+
+    if (parseInt(window.innerWidth) <= 1440){
+
+        carouselItem.css('margin', '0 15px');
+        carouselImage.css('height', 'auto');
+
+        carousel.slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+                },
+                {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+                },
+                {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                }
+            ]
+        });
+    };
+
+    $(".btn").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#form").offset().top
+        }, 1000);
+    });
+    $(".nav__link-1").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#about-us").offset().top
+        }, 1000);
+    });
+    $(".nav__link-2").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#services").offset().top
+        }, 1000);
+    });
+    $(".nav__link-3").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#elephant").offset().top
+        }, 1000);
+    });
+    $(".nav__link-4").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#blog").offset().top
+        }, 1000);
+    });
+    $(".nav__link-5").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+
+    $('#form').trigger('reset');
+        $(function() {
+          'use strict';
+          $('#form').on('submit', function(e) {
+            e.preventDefault();
+            $.ajax({
+              url: 'send.php',
+              type: 'POST',
+              contentType: false,
+              processData: false,
+              data: new FormData(this),
+              success: function(msg) {
+                console.log(msg);
+                if (msg == 'ok') {
+                  alert('Спасибо за сообщение! В скором времени мы свяжемся с вами.');
+                  $('#form').trigger('reset'); // очистка формы
+                } else {
+                  alert('Ошибка');
+                }
+              }
+            });
+          });
+        });
+});
---
+++ send.php
@@ -0,0 +1,64 @@
+<?php
+// Файлы phpmailer
+require 'php/PHPMailer.php';
+require 'php/SMTP.php';
+require 'php/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $msg = "ok";
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";                                          
+    $mail->SMTPAuth   = true;
+
+    // Настройки вашей почты
+    $mail->Host       = ''; // SMTP сервера GMAIL
+    $mail->Username   = ''; // Логин на почте
+    $mail->Password   = ''; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('slon@elephant.ru', 'Заказ слона'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('');  
+    $mail->addAddress(''); // Ещё один, если нужен
+
+//     // Прикрипление файлов к письму
+// if (!empty($_FILES['myfile']['name'][0])) {
+//     for ($ct = 0; $ct < count($_FILES['myfile']['tmp_name']); $ct++) {
+//         $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['myfile']['name'][$ct]));
+//         $filename = $_FILES['myfile']['name'][$ct];
+//         if (move_uploaded_file($_FILES['myfile']['tmp_name'][$ct], $uploadfile)) {
+//             $mail->addAttachment($uploadfile, $filename);
+//         } else {
+//             $msg .= 'Неудалось прикрепить файл ' . $uploadfile;
+//         }
+//     }   
+// }
+
+        // -----------------------
+        // Само письмо
+        // -----------------------
+        $mail->isHTML(true);
+    
+        $mail->Subject = 'Заказ слона';
+        $mail->Body    = "<b>Имя:</b> $name <br>
+        <b>Почта:</b> $email<br><br>
+        <b>Сообщение:</b><br>$text";
+
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {
+    echo "$msg";
+} else {
+echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+}
+
+} catch (Exception $e) {
+    echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
